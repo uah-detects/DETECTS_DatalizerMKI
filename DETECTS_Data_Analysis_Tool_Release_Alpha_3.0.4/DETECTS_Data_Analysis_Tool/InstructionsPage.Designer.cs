0bae9a3476465d51f4c19d23a4bf9944f4ddb1fd
Samuel <Malestel-48@hotmail.com>
2021-12-08 13:19:30 -0600

    Fixed Push

---
+++ DETECTS_Data_Analysis_Tool_Release_Alpha_3.0.4/DETECTS_Data_Analysis_Tool/InstructionsPage.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace DETECTS_Data_Analysis_Tool
+{
+    partial class InstructionsPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstructionsPage));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.backButton = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Instructions:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(376, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1. Clean your data. (Ref. Notebook pgs. 61 - 64)\r\n    See video for further instr" +
+    "uctions on proper data format.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "2. Navigate to the Main Menu.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "3. Click the Graph button.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(243, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "4. Drag your CSV file into the textbox.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(282, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "5. Select the points you would like to graph.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 242);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "6. Click Graph";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(532, 354);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(263, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "DETECTS Datalizer Release alpha 3.0.4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 337);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(230, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Samuel Strong - sts0020@uah.edu";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 286);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(293, 17);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Please report any bugs or software issues to:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 303);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(234, 17);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Andrew Howard - ah0061@uah.edu";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 320);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(260, 17);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Hannah Jimmerson - hej0006@uah.edu";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 354);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(270, 17);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "UAH DETECTS Team - detects@uah.edu";
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(662, 303);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(123, 40);
+            this.backButton.TabIndex = 13;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(10, 53);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(107, 25);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "Graphing:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(402, 53);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(250, 25);
+            this.label15.TabIndex = 15;
+            this.label15.Text = "Calculating Ascent Rate:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(404, 84);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(376, 34);
+            this.label16.TabIndex = 16;
+            this.label16.Text = "1. Clean your data. (Ref. Notebook pgs. 61 - 64)\r\n    See video for further instr" +
+    "uctions on proper data format.\r\n";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(404, 127);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(197, 17);
+            this.label17.TabIndex = 17;
+            this.label17.Text = "2. Navigate to the Main Menu.";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(404, 154);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(268, 17);
+            this.label18.TabIndex = 18;
+            this.label18.Text = "3. Click the Calculate Ascent Rate button.";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(404, 182);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(243, 17);
+            this.label19.TabIndex = 19;
+            this.label19.Text = "4. Drag your CSV file into the textbox.";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(404, 209);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(282, 17);
+            this.label20.TabIndex = 20;
+            this.label20.Text = "5. Select the points you would like to graph.";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(404, 269);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(340, 34);
+            this.label21.TabIndex = 21;
+            this.label21.Text = "7. Enter the name for your file. (Make sure to save it \r\n    to a location you ca" +
+    "n easily access.)";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(404, 238);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(115, 17);
+            this.label22.TabIndex = 22;
+            this.label22.Text = "6. Click Calculate";
+            // 
+            // InstructionsPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 374);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "InstructionsPage";
+            this.Text = "DETECTS Datalizer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.InstructionPage_FormClosed);
+            this.Load += new System.EventHandler(this.InstructionsPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+    }
+}
---
+++ DETECTS_Data_Analysis_Tool_Release_Alpha_3.0.4/DETECTS_Data_Analysis_Tool/fileManipulation.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DETECTS_Data_Analysis_Tool
+{
+    class fileManipulation
+    {
+
+        public List<string> readHeaderInput(string filePath)
+        {
+            List<string> data = new List<string>();
+
+            if (filePath != null)
+            {
+                try
+                {
+                    // Open the file to read from.
+                    using (StreamReader sr = File.OpenText(filePath))
+                    {
+
+                        string s;
+                        s = sr.ReadLine();
+                        string[] values = s.Split(',');                               // parsing the data
+                        for (int i = 0; i < values.Length; i++)
+                        {
+                            data.Add(values[i]);
+                        }
+                        sr.Close();
+
+                    }
+                    
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Path denied. Please pull the file out of the directory.");
+                    return null;
+                }
+
+            }
+            else
+            {
+                data = null;
+            }
+
+            return data;
+        }
+
+        // function reads in a file and outputs the data as a list of type dataPointsRaw
+        public List<dataPointsRaw> readInput(string filePath, int xIndex, int yIndex)
+        {
+            List<dataPointsRaw> data = new List<dataPointsRaw>();
+
+            if (filePath != null)
+            {
+                try
+                {
+                    // Open the file to read from.
+                    using (StreamReader sr = File.OpenText(filePath))
+                    {
+                        string s;
+                        while ((s = sr.ReadLine()) != null)                               // looping through the data file until the end
+                        {
+                            string[] values = s.Split(',');                               // parsing the data
+                            dataPointsRaw d = new dataPointsRaw(values[xIndex], values[yIndex]);
+                            data.Add(d);                                                  // adding the data to the list
+
+                        }
+                        sr.Close();
+
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Path denied. Please pull the file out of the directory.");
+                    return null;
+                }
+
+            }
+            else
+            {
+                data = null;
+            }
+
+            return data;
+        }
+
+        public bool isHeaderExist(List <dataPointsRaw> data)
+        {
+            AscentRateCalculator calc = new AscentRateCalculator();
+            dataPointsRaw headerLine;
+            pointIsDate list;
+            double i;
+            headerLine = data.First();
+
+            list = calc.isPointDateTime(data);            //testing if any columns are a DateTime
+
+            if(!list.xData && !list.yData)               // testing if DateTime
+            {
+                if (!double.TryParse(headerLine.xData, out i) && !double.TryParse(headerLine.xData, out i))      //testing if number
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void addColumnToCSVFile(string header, List<double> dataList, string originalFilePath)
+        {
+            if (originalFilePath != null)
+            {
+                SaveFileDialog saveCSV = new SaveFileDialog();
+                saveCSV.Title = "Save text Files";
+                saveCSV.CheckFileExists = false;
+                saveCSV.CheckPathExists = false;
+                saveCSV.CreatePrompt = true;
+                saveCSV.DefaultExt = "csv";
+                saveCSV.Filter = "CSV Files (*.csv)|*.csv|Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveCSV.FilterIndex = 1;
+                saveCSV.RestoreDirectory = true;
+                if (saveCSV.ShowDialog() == DialogResult.OK)
+                {
+                    using (Stream st = File.Open(saveCSV.FileName, FileMode.CreateNew))
+                    {
+                        StreamWriter sw = new System.IO.StreamWriter(st);
+                        try
+                        {
+                            // Open the file to read from.
+                            using (StreamReader sr = File.OpenText(originalFilePath))
+                            {
+                                bool first = true;
+                                string s;
+                                while ((s = sr.ReadLine()) != null)                               // looping through the data file until the end
+                                {
+                                    if(first)
+                                    {
+                                        s = s + "," + header + "\n";
+                                        sw.Write(s);
+                                        first = false;
+                                    }
+                                    else
+                                    {
+
+                                        s = s + "," + dataList.First() + "\n";
+                                        dataList.RemoveAt(0);
+                                        sw.Write(s);
+                                        sw.Flush();                                                  //Cleaning the stream buffer
+                                    }
+
+                                }
+                                first = true;
+                                sr.Close();
+
+                            }
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Error: Path denied. Please pull the file out of the directory.");
+                            return;
+                        }
+
+                        st.Close();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error: File Not Found.");
+                return;
+            }
+        }
+
+    }
+}
